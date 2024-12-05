f6c462517820fcab6e5c065c849e287675bdca6a
Yash Lakhwani <yashlakhwani.important@gmail.com>
2024-12-06 03:49:18 +0530

    learnt about models

--- djangoTutorial/chai/models.py
+++ djangoTutorial/chai/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -19,3 +20,30 @@ class ChaiVariety(models.Model):
     def __str__(self) -> str:
         return self.name
     
+# one to many
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete = models.CASCADE, related_name = 'reviews')
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateField(default = timezone.now)
+    
+    def __str__(self) -> str:
+        return f'{self.user.username} review for {self.chai.name}'
+    
+
+# Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    chai_variety = models.ManyToManyField(ChaiVariety, related_name='stores')
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+# one to one
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name='certificate')
